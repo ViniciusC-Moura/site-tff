48b9f9869cdf191a1800ddaf36df2c82794d2a86
ViniciusC-Moura <vinicius.cavalcanti@escolar.ifrn.edu.br>
2026-08-18 14:42:52 -0300

    Modelos implementados e funcionando no debug

--- src/core/models/Esporte.py
+++ src/core/models/Esporte.py
@@ -1,7 +1,8 @@
 from django.db import models 
 
 class Esporte(models.Model):
+    nome = models.CharField(max_length=100)
     tecnico = models.CharField(max_length=255)
 
     def __str__(self):
-        return f"Esporte - {self.tecnico}"
+        return f"Esporte - {self.nome}"
